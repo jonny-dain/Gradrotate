2ea1207bb892e90be32fb52b9c652704c99ab135
Jonny Dain <jonnydain@Jonnys-MacBook-Air.local>
2022-11-09 15:52:10 +0000

    updated requirements.txt and .gitignore

--- gradRotate/local.py
+++ gradRotate/local.py
@@ -12,42 +12,30 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 
 import sys
-import environ
 import dj_database_url
 
 from django.core.management.utils import get_random_secret_key
 
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env()
-environ.Env.read_env ()
-
-environ.Env.read_env(os.path.join(BASE_DIR , '.env'))
-
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-SECRET_KEY = env('SECRET_KEY')
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-DEBUG = False
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
-
-#ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -102,22 +90,6 @@ DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
-DATABASES = {
-'default' : {
-'ENGINE ': 'django.db.backends.postgresql_psycopg2 ',
-'NAME': env('DATABASE_NAME '),
-'USER': env('DATABASE_USER '),
-'PASSWORD ': env('DATABASE_PASS '),
-'HOST': env('DATABASE_HOST '),
-'PORT': '5432',
-}
-}
-
-
-
-
-
 #if DEVELOPMENT_MODE is True:
 #    DATABASES = {
 #        "default": {
@@ -132,12 +104,12 @@ DATABASES = {
 #        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 #    }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation

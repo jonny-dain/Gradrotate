1710b23cd755e0d5d95afd074e50cb3de1d81bdb
Jonny Dain <jonnydain@Jonnys-MacBook-Air.local>
2022-11-10 15:48:26 +0000

    updated CI, added deploy/db.template

--- gradRotate/settings.py
+++ gradRotate/settings.py
@@ -105,7 +105,7 @@ DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 DATABASES = {
 'default' : {
-'ENGINE ': 'django.db.backends.postgresql_psycopg2 ',
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
 'NAME': env('DATABASE_NAME'),
 'USER': env('DATABASE_USER'),
 'PASSWORD ': env('DATABASE_PASS'),

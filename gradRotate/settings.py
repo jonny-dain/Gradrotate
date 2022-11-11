2b0ad38867c6d5503bd7b77720e7ef5a5b38e172
Jonny Dain <jonnydain@Jonnys-MacBook-Air.local>
2022-11-11 13:34:19 +0000

    updated CI, added deploy/db.template

--- gradRotate/settings.py
+++ gradRotate/settings.py
@@ -24,7 +24,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read_env ()
+environ.Env.read_env()
 
 environ.Env.read_env(os.path.join(BASE_DIR , '.env'))
 

4f4a29d3376751a8930e579baf2856f5c2f749a9
Jonny Dain <jonnydain@Jonnys-MacBook-Air.local>
2022-12-14 15:21:08 +0000

    Added dynamic dashboard

--- dashboard/dashboard_info.py
+++ dashboard/dashboard_info.py
@@ -43,7 +43,20 @@ def job_skills(jobs):
    
     return job_all_skills
 
+def get_intern_preferences(request):
+    intern_preferences = InternPreference.objects.filter(intern = request.user.intern)
+    
+    #Can be placed in function
+    
+    intern_preference_dictionary = {}
+    for intern_preference in intern_preferences:
+        
+        intern_preference_dictionary[intern_preference] = intern_preference.preference
 
+    intern_preference_dictionary = dict(sorted(intern_preference_dictionary.items(), key=lambda x:x[1]))
+    intern_preferences = list(intern_preference_dictionary.keys())
+    
+    return intern_preferences
 
 def job_ordered_list(request):
     #create a preference dictionary based on the skill similarities [digital degree, TPO, Change Manager]
@@ -105,5 +118,70 @@ def job_ordered_list(request):
 
     job_dictionary = dict(job_dictionary)
     
+
+    return job_dictionary
+
+
+
+
+def job_ordered_preference_list(request):
+    #create a preference dictionary based on the preferences {<Job: Digital degree>: 57, <Job: TPO>: 43, <Job: Change manager>: 29}
+    
+    jobs = Job.objects.all()
+    intern = request.user.intern
+
+
+
+    intern_dictionary = {}
+    preferences = intern.internpreference_set.all()
+    for preference in preferences:
+
+        intern_dictionary[preference.job] = preference.preference
+
+    intern_dictionary = sorted(intern_dictionary.items(), key=lambda x:x[1])
+    intern_dictionary = dict(intern_dictionary)
+    
+    computing_skills_intern = intern.computing_skills.all()
+    analytic_skills_intern = intern.analytic_skills.all()
+    marketing_skills_intern = intern.marketing_skills.all()
+    management_skills_intern = intern.management_skills.all()
+    leadership_skills_intern = intern.leadership_skills.all()
+    business_skills_intern = intern.business_skills.all()
+    admin_skills_intern = intern.admin_skills.all()
+
+    job_dictionary = {}
+    for job in list(intern_dictionary.keys()):
+        similarity_weighting = 0
+        
+        computing_skills_job = job.computing_skills.all()
+        analytic_skills_job = job.analytic_skills.all()
+        marketing_skills_job = job.marketing_skills.all()
+        management_skills_job = job.management_skills.all()
+        leadership_skills_job = job.leadership_skills.all()
+        business_skills_job = job.business_skills.all()
+        admin_skills_job = job.admin_skills.all()
+
+        similarity_weighting = len(list(set(computing_skills_job).intersection(computing_skills_intern)))
+        similarity_weighting += len(list(set(analytic_skills_job).intersection(analytic_skills_intern)))
+        similarity_weighting += len(list(set(marketing_skills_job).intersection(marketing_skills_intern)))
+        similarity_weighting += len(list(set(management_skills_job).intersection(management_skills_intern)))
+        similarity_weighting += len(list(set(leadership_skills_job).intersection(leadership_skills_intern)))
+        similarity_weighting += len(list(set(business_skills_job).intersection(business_skills_intern)))
+        similarity_weighting += len(list(set(admin_skills_job).intersection(admin_skills_intern)))
+
+        #This calculates the weighting...
+       #If the locations are the same add one to the weighting - total max is now 6
+        if job.job_location == intern.job_location:
+            similarity_weighting += 1
+        
+        #If remote is the same add one to the weighting - total max is now 7
+        if job.remote == intern.remote:
+            similarity_weighting += 1
+
+        #Section for preferred wage
+        similarity_weighting = round(similarity_weighting/7 * 100)
+
+        job_dictionary[job] = similarity_weighting
+
 
     return job_dictionary
--- dashboard/views.py
+++ dashboard/views.py
@@ -40,168 +40,48 @@ class BaseCheckFormSet(BaseFormSet):
 #@login_required(login_url='../login')
 #@allowed_users(allowed_roles=['Intern'])
 def preference(request):
-    #jobs = Job.objects.all()
+
+    #creates list for new interns based on skills before
     job_with_percentage = job_ordered_list(request)
 
-    jobs_percentage = list(job_with_percentage.values())
+    
+    
+    
     jobs = list(job_with_percentage.keys())
 
-    jobs_count = len(jobs)
-
+    
+    
     counter = 0
     for job in jobs:
         counter += 1
         if not InternPreference.objects.filter(intern = request.user.intern, job = job).exists():
             InternPreference.objects.create(intern = request.user.intern, job = job, preference = counter)
-
-
-    #HAS TO CHANGE
-    intern_preferences = InternPreference.objects.filter(intern = request.user.intern)
-    
-    #Can be placed in function
-    
-    intern_preference_dictionary = {}
-    for intern_preference in intern_preferences:
-        
-        intern_preference_dictionary[intern_preference] = intern_preference.preference
-
-    intern_preference_dictionary = dict(sorted(intern_preference_dictionary.items(), key=lambda x:x[1]))
-
-
-    intern_preferences = list(intern_preference_dictionary.keys())
     
 
-    #sort based on preferences..
+
+    #orders list based on the prefence list
+    job_with_percentage = job_ordered_preference_list(request)
+
+    #Gathers information on the jobs - the percentage of each job, total jobs and jobs count
+    jobs_percentage = list(job_with_percentage.values())
+    jobs = list(job_with_percentage.keys())
+    jobs_count = len(jobs)
 
 
-
-
-
-
-
-
-    OrderFormSet = inlineformset_factory(Intern, InternPreference, fields = ('job', 'preference'), extra = jobs_count,max_num=jobs_count)
-    intern = request.user.intern
+    #gets all the intern job preferences in a dictionary
+    intern_preferences = get_intern_preferences(request)
     
-    formset = OrderFormSet(instance = intern)
 
-
-
-
-    
     #This gets all the skills for each of the jobs and gives a %
     all_job_skills = job_skills(jobs = jobs)
 
    
 
     #Zipped the form and jobs
-    form_job = zip(formset, jobs)
+    form_job = zip(jobs, all_job_skills, jobs_percentage)
 
-
-    form_job2 = zip(jobs, all_job_skills, jobs_percentage)
-    
-    print('here')
-
-    #remove the formset context
-    #context = { 'jobs_count': range(jobs_count), 'formset': formset, 'zippedlist': form_job,'zippedlist2':form_job2}
-    context = { 'jobs_count': range(jobs_count), 'formset': formset, 'intern_preferences': intern_preferences,'zippedlist2':form_job2}
+    context = { 'jobs_count': range(jobs_count), 'intern_preferences': intern_preferences,'zippedlist2':form_job}
     return render(request, 'dashboard/dashboard.html', context)
-
-
-
-
-
-
-
-
-    if request.method == 'POST':
-        #form = InternPreferenceForm(request.POST)
-        formset = OrderFormSet(request.POST, instance = intern)
-
-        counter = 0
-        
-        for form in formset:
-            form.job = jobs[counter]
-            #form.set_job(jobs[counter])
-            counter = counter + 1
-        
-        
-        if formset.is_valid():
-            #Validation of form
-            jobs_list = []
-            preference_list = []
-            errors_list = []
-
-            counter = 0
-
-            for form in formset:
-            
-                
-                #form.cleaned_data['job'] = jobs[counter]
-                #job = form.cleaned_data.get('job')
-                preference_number = form.cleaned_data.get('preference')
-                #print(form.job)
-                #print(preference_number)
-                #form.set_job(jobs[counter])
-                counter = counter + 1
-
-
-                #Set job to counter form.job = jobs.get[loop_counter].. then the loop has to contain the job name
-                #zip form with list of jobs https://stackoverflow.com/questions/12684128/looping-through-two-objects-in-a-django-template
-                
-
-                #if job in jobs_list:
-                    #errors_list.append('All jobs must have a preference')
-                    
-                if preference_number in preference_list:
-                    errors_list.append('All jobs must have different preferences')
-                    
-                #jobs_list.append(job)
-                preference_list.append(preference_number)
-            if len(errors_list) > 0:
-                
-                context = {'jobs' :jobs, 'jobs_count': range(jobs_count), 'formset': formset, 'errorlist':errors_list, 'zippedlist': form_job}
-                return render(request, 'dashboard/dashboard.html', context)
-            else:
-                #formset doesn't save job
-                counter = 0
-                for form in formset:
-                    preference_number = form.cleaned_data.get('preference')
-                    
-                    
-
-                    #this might have fixed it....
-
-                    obj = form.save(commit=False)
-                    obj.job = jobs[counter]
-                    obj.save()
-                    counter = counter + 1
-
-                    
-                    
-                    #FORM.preference doesnt exist?!?
-
-
-                    #print(form.preference)
-                #formset.save()
-
-                #check if done
-                
-                intern.progress = 3
-                
-                intern.save()
-
-
-                return HttpResponse("done")
-        else:
-            
-            print('failed')
-
-    return render(request, 'dashboard/dashboard.html', context)
-    
-
-
-
 
 
 
@@ -219,10 +99,15 @@ def sort(request):
         intern_preferences.append(intern_preference)
 
 
+    job_with_percentage = job_ordered_preference_list(request)
 
-    #return redirect('../../dashboard')
-    context = {'intern_preferences': intern_preferences}
+    jobs_percentage = list(job_with_percentage.values())
+    jobs = list(job_with_percentage.keys())
+    jobs_count = len(jobs)
+
+    all_job_skills = job_skills(jobs = jobs)
+    form_job2 = zip(jobs, all_job_skills, jobs_percentage)
     
-    #return redirect('../../../dashboard')
+    context = { 'jobs_count': range(jobs_count), 'intern_preferences': intern_preferences,'zippedlist2':form_job2}
 
     return render(request, 'dashboard/dashboard-pref.html', context)
